962ea2a9e524e057005cb8c084bb96774a39c417
Xianming Zhong <chinesedfan@qq.com>
2017-02-24 14:24:18 +0800

    serve static files

--- server/router.js
+++ server/router.js
@@ -4,6 +4,11 @@ const path = require('path');
 const send = require('koa-send');
 const router = require('koa-router')();
 
+router.get('/dist/:file', function*(next) {
+    yield send(this, this.params.file, {
+        root: path.resolve(__dirname, '../dist')
+    });
+});
 router.get('/', function*(next) {
     yield send(this, 'index.html', {
         root: path.resolve(__dirname, '../web/pages')
