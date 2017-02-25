37f81ec8a5c71a9a5617d2f01a24906958672411
Xianming Zhong <chinesedfan@qq.com>
2017-02-25 17:04:44 +0800

    server: join specified room, emit to specified room

--- server/api/rewrite.js
+++ server/api/rewrite.js
@@ -1,21 +1,27 @@
 'use strict';
 
 const url = require('url');
+const Cookie = require('../../common/cookie');
 
 module.exports = function*(next) {
     // TODO: check the mock config to determine whether request or mock
     // console.log(this.query.url, this.query.cookie, this.query.reqtype);
 
     const parsed = url.parse(decodeURIComponent(this.query.url), true, true);
+    const clientID = Cookie.getCookieItem(this.query.cookie, Cookie.KEY_CLIENT_ID);
+    if (!clientID) {
+        this.body = 'no clientID, ignored';
+        return;
+    }
 
-    this.app.io.emit('mock-request-start', {
+    this.app.io.sockets.in(clientID).emit('mock-request-start', {
         method: this.request.method,
         host: parsed.host,
         pathname: parsed.pathname,
         timestamp: getNowInHHMMSS()
     });
 
-    this.body = 'rewrite success!';
+    this.body = `${clientID}: rewrite success!`;
 };
 
 function getNowInHHMMSS() {
--- server/io.js
+++ server/io.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Cookie = require('../common/cookie');
+
 module.exports = function(server) {
     const io = require('socket.io')(server);
     io.on('connection', onConnection);
@@ -7,9 +9,18 @@ module.exports = function(server) {
 };
 
 function onConnection(socket) {
-    console.log('connected...');
+    console.log(`[${socket.id}]connected...`);
 
     socket.on('disconnect', function() {
-        console.log(`...disconnect!`);
+        console.log(`[${socket.id}]...disconnect!`);
     });
+
+    const clientID = Cookie.getCookieItem(socket.request.headers.cookie, Cookie.KEY_CLIENT_ID);
+    if (!clientID) {
+        console.log(`[${socket.id}]...no clientID to join`);
+        return;
+    }
+
+    socket.join(clientID);
+    console.log(`[${socket.id}]...join ${clientID}`);
 }
