273fcaa09103c0703d704d3fb086bb965133bf0c
Xianming Zhong <chinesedfan@qq.com>
2017-03-06 11:17:56 +0800

    append special url params

---
+++ common/urlparams.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.URL_PARAM_MOCK = '__mock';
+exports.URL_PARAM_HOST = '__host';
+exports.URL_PARAM_CLIENT_ID = '__clientid';
--- web/pages/qrcode.js
+++ web/pages/qrcode.js
@@ -4,6 +4,10 @@ import './qrcode.less';
 import Vue from 'vue';
 
 import qrcode from 'vue2-qrcode/src/qrcode.vue';
+import {KEY_CLIENT_ID, getCookieItem} from '../../common/cookie.js';
+import * as UrlParams from '../../common/urlparams.js';
+
+import url from 'url';
 
 new Vue({
     el: '#vue-container',
@@ -13,15 +17,24 @@ new Vue({
     data() {
         return {
             url: 'https://www.dianping.com',
-            size: 500
+            size: 500,
+            clientID: getCookieItem(document.cookie, KEY_CLIENT_ID)
         };
     },
     methods: {
         onInput(e) {
-            const url = e.currentTarget.value;
-            if (!url) return;
+            const val = e.currentTarget.value;
+            if (!val) return;
 
-            this.url = url;
+            const parsed = url.parse(val, true, true);
+            parsed.search = ''; // must clear
+            parsed.query = {
+                [UrlParams.URL_PARAM_MOCK]: 1,
+                [UrlParams.URL_PARAM_HOST]: location.protocol + '//' + location.host,
+                [UrlParams.URL_PARAM_CLIENT_ID]: this.clientID,
+                ...parsed.query
+            };
+            this.url = url.format(parsed);
         }
     }
 });
