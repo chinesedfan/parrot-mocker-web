f8548c1054c8c4af79565aae175f243693fba88b
Xianming Zhong <chinesedfan@qq.com>
2017-04-25 19:33:49 +0800

    testxhr returns debug data

--- server/api/testxhr.js
+++ server/api/testxhr.js
@@ -3,6 +3,11 @@
 module.exports = function*(next) {
     this.body = {
         code: 200,
-        msg: 'good xhr'
+        msg: 'good xhr',
+        data: {
+            method: this.request.method,
+            requestHeaders: this.request.headers,
+            requestData: this.request.body
+        }
     };
 };
