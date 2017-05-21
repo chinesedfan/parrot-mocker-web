e69f095ca5de5427851815c54343572bf007f04f
Xianming Zhong <chinesedfan@qq.com>
2017-05-21 17:25:41 +0800

    support to disable the mock by qrcode, close #6

--- web/pages/qrcode.js
+++ web/pages/qrcode.js
@@ -16,28 +16,48 @@ new Vue({
     },
     data() {
         return {
+            val: 'https://www.dianping.com',
             url: 'https://www.dianping.com',
             size: 500,
-            clientID: getCookieItem(document.cookie, KEY_CLIENT_ID)
+            clientID: getCookieItem(document.cookie, KEY_CLIENT_ID),
+            // options
+            isReset: false
         };
     },
     methods: {
         onInput(e) {
-            const val = e.currentTarget.value;
-            if (!val) return;
+            this.val = e.currentTarget.value;
+            this.updateUrl();
+        },
+        onResetClicked() {
+            this.isReset = !this.isReset;
+            this.updateUrl();
+        },
+        onQrcodeClicked() {
+            window.open(this.url, '_blank');
+        },
 
-            const parsed = url.parse(val, true, true);
+        updateUrl() {
+            if (!this.val) return;
+
+            const parsed = url.parse(this.val, true, true);
             parsed.search = ''; // must clear
             parsed.query = {
-                [UrlParams.URL_PARAM_MOCK]: 1,
-                [UrlParams.URL_PARAM_HOST]: location.protocol + '//' + location.host,
-                [UrlParams.URL_PARAM_CLIENT_ID]: this.clientID,
+                ...this.getExtraParams(),
                 ...parsed.query
             };
             this.url = url.format(parsed);
         },
-        onQrcodeClicked() {
-            window.open(this.url, '_blank');
+        getExtraParams() {
+            const params = {
+                [UrlParams.URL_PARAM_MOCK]: this.isReset ? '0' : '1'
+            };
+
+            if (!this.isReset) {
+                params[UrlParams.URL_PARAM_HOST] = location.protocol + '//' + location.host;
+                params[UrlParams.URL_PARAM_CLIENT_ID] = this.clientID;
+            }
+            return params;
         }
     }
 });
