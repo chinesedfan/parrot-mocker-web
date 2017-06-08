fbc18127fb22fd82b9bdf7488458f00160600f7a
Xianming Zhong <chinesedfan@qq.com>
2017-06-08 15:13:52 +0800

    return json

--- server/api/setclientid.js
+++ server/api/setclientid.js
@@ -6,12 +6,21 @@ const UrlParams = require('../../common/urlparams');
 module.exports = function*(next) {
     const clientID = this.query[UrlParams.URL_PARAM_CLIENT_ID];
     if (!clientID) {
-        this.body = 'no clientID, ignored';
+        this.body = {
+            code: 500,
+            msg: 'no clientID, ignored'
+        };
         return;
     }
 
     // keep the same with web UI, host-only cookie
     const cookieStr = Cookie.generateCookieItem(Cookie.KEY_CLIENT_ID, clientID, Infinity, '/', '');
     this.response.set('set-cookie', cookieStr);
-    this.body = 'updated as ' + clientID;
+    this.body = {
+        code: 200,
+        msg: 'success',
+        data: {
+            clientID
+        }
+    };
 };
