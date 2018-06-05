15dfffe87b07e6ada2f3599109c21636c2d88534
Xianming Zhong <chinesedfan@qq.com>
2018-06-05 22:39:26 +0800

    ignore some files to cover

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    coveragePathIgnorePatterns: [
+        '<rootDir>/common/cookies.js',
+        '<rootDir>/server/lib/node-fetch.js'
+    ],
     testMatch: [
         '<rootDir>/test/*.test.js'
     ],
