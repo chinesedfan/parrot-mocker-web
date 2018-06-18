852388c50d3a16dc380f5fdc9fb609d5d6ba8ca6
Xianming Zhong <chinesedfan@qq.com>
2018-06-18 12:51:13 +0800

    test co-body limit

--- test/testxhr.test.js
+++ test/testxhr.test.js
@@ -35,4 +35,33 @@ describe('/api/testxhr', () => {
             b: 2
         });
     });
+    it('should handle large json POST request', async () => {
+        // For co-body, limit for json data is 1mb, but we should leave some spaces for headers
+        const kb = 1023;
+        const postData = {
+            payload: Array(kb * 1024 / 4).fill('a')
+        };
+        const {method, requestData} = await request(app.callback())
+            .post('/api/testxhr')
+            .send(postData)
+            .then((res) => res.body.data);
+
+        expect(method).toEqual('POST');
+        expect(requestData).toEqual(postData);
+    });
+    it('should handle large form POST request', async () => {
+        // For co-body, limit for form data is 56kb, but we should leave some spaces for headers
+        const kb = 55;
+        const postData = {
+            payload: Array(kb * 1024).fill('a').join('')
+        };
+        const {method, requestData} = await request(app.callback())
+            .post('/api/testxhr')
+            .type('form')
+            .send(postData)
+            .then((res) => res.body.data);
+
+        expect(method).toEqual('POST');
+        expect(requestData).toEqual(postData);
+    });
 });
